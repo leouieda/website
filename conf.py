dbb7d39e900ea64a3a58572952ce0586b73fa325
Leonardo Uieda <leouieda@gmail.com>
2013-09-20 18:08:55 -0300

    Initial empty Pelican site
    Uses the 'simple' theme. Basically an empty theme so that I can
    customize my own. Renamed pelicanconf.py to just conf.py because it
    looks better. Also removed unsed (ftp, ssh, etc) rules from Makefile.

---
+++ conf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Leonardo Uieda'
+SITENAME = u'Earth, in a computer'
+SITEURL = ''
+
+TIMEZONE = 'Brazil'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+THEME = 'theme'
+
+# Blogroll
+LINKS =  (('About', '/about.html'),
+        ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
