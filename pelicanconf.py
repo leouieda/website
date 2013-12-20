ca06c89723f1132059da3a34e1d29d8a4d774304
Leonardo Uieda <leouieda@gmail.com>
2013-12-20 16:27:12 -0200

    Added more social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 import time
 
 AUTHOR = u'Leonardo Uieda'
-SITESUBTITLE = u'Geophysics, science, inverse problems, and software'
-SITENAME = u'&lt;insert witty title&gt; - {}'.format(SITESUBTITLE)
+SITESUBTITLE = u'Geophysics, inverse problems, science, and software'
+SITENAME = u'&lt;insert witty geoscience title&gt; - {}'.format(SITESUBTITLE)
 #SITEURL = 'http://leouieda.github.io'
 SITEURL = ''
 COPYRIGHT_NOTICE = """
@@ -40,6 +40,11 @@ TWITTER_USERNAME = 'leouieda'
 SOCIAL = (
     ('github', 'https://github.com/leouieda'),
     ('twitter', 'https://twitter.com/leouieda'),
-    ('google+', 'https://plus.google.com/+LeonardoUieda'),
+    ('google-plus', 'https://plus.google.com/+LeonardoUieda'),
+    ('linkedin', 'http://www.linkedin.com/in/uieda'),
+    ('figshare', 'http://figshare.com/authors/Leonardo%20Uieda/97471'),
+    ('orcid', 'http://orcid.org/0000-0001-6123-9515'),
+    ('google-scholar',
+        'http://scholar.google.com.br/citations?user=qfmPrUEAAAAJ'),
 )
 
