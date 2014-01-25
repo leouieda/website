1c86059ac662dbaa50569e942526c772660442d1
Leonardo Uieda <leouieda@gmail.com>
2014-01-25 15:24:56 -0200

    Changed blog name to metageophysics

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@ import os
 
 AUTHOR = u'Leonardo Uieda'
 
-SITENAME = u'Leonardo Uieda'
+SITENAME = u'Metageophysics'
 SITESUBTITLE = u'Geophysics, inverse problems, science, and software'
 SITEKEYWORDS = u'geophysics, earth, earthscience, science, foss'
 SITEURL = ''
