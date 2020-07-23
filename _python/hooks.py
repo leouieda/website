67684dae2b528ddde71ad87e1e17b3796837159a
Leonardo Uieda <leouieda@gmail.com>
2020-07-23 17:54:31 +0100

    Add copyright to the site footer

--- _python/hooks.py
+++ _python/hooks.py
@@ -1,6 +1,7 @@
 """
 Pre-processing of the page metadata before rendering.
 """
+from datetime import datetime
 
 
 def process_info(info, site):
@@ -8,9 +9,12 @@ def process_info(info, site):
     Alter the page metadata before rendering.
     """
     # Urubu doesn't split the 'tags' into multiple strings
-    if 'tags' in info:
-        if isinstance(info['tags'], str):
-            info['tags'] = info['tags'].split(', ')
+    if "tags" in info:
+        if isinstance(info["tags"], str):
+            info["tags"] = info["tags"].split(", ")
     # Identify to which folder the item belongs (paper, blog, etc)
-    if 'type' not in info:
-        info['type'] = '/{}'.format(info['id'].split('/')[1])
+    if "type" not in info:
+        info["type"] = "/{}".format(info["id"].split("/")[1])
+    # Add the current date to the site metadata
+    if "now" not in site:
+        site["now"] = datetime.utcnow()
