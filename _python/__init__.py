075d1519fe124f6afc7c1e47b3277c19ec506574
Leonardo Uieda <leouieda@gmail.com>
2021-09-26 21:21:34 +0100

    Remove old files that won't stick around

--- _python/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Custom Jinja filters and preprocessors.
-"""
-from .filters import filters
-from .hooks import process_info
--- rss.md
+++
@@ -1,7 +0,0 @@
----
-title: RSS feed for blog posts
-layout: rss
-content:
-    - blog
-saveas: rss.xml
----
